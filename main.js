464b1c49064277fe7ecf09bd0861d3f493d597bb
Peter <pndungu54@gmail.com>
2018-10-11 10:57:01 +0300

    Implement reviews

--- main.js
+++ main.js
@@ -18,19 +18,6 @@ const contextMenu = require('./app/windows/maincontextmenu');
 
 const api = require('./api/api');
 
-// const express = require('express');
-
-// const bodyParser = require('body-parser');
-
-// const mongoose = require('mongoose');
-
-// const morgan = require('morgan');
-
-// const config = require('config');
-
-// const api = express();
-
-
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 let splashWindow = require('./app/windows/splashWindow');
@@ -44,33 +31,6 @@ let adminDashboard = require('./app/windows/adminDashboard');
 let accountWindow = require('./app/windows/userCreation');
 
 let hrWindow = require('./app/windows/hrWindow');
-
-// // connect to mongodb
-// mongoose.connect(config.DBHost);
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error'));
-
-// //don't show the log when it is test
-// if (config.util.getEnv('NODE_ENV') !== 'test') {
-//     //use morgan to log at command line
-//     api.use(morgan('combined')); //'combined' outputs the Apache style LOGs
-// }
-
-// mongoose.Promise = global.Promise;
-
-// api.use(bodyParser.json());
-
-// // initialize routes
-// api.use('/api', require('./api/routes/router'));
-
-// // listen for requests
-// api.listen(config.Port || 8000, () => {
-//     console.log('Environment ' + process.env);
-//     console.log('Listening for requests on port ' + config.Port + '.....');
-// });
-
-// // for testing
-// module.exports = api;
 
 function createWindow(screen) {
     // create the browser window
