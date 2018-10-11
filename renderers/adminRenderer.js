6d5f38854d077c9f8990dcba93320f3d8e5e1c44
Peter <pndungu54@gmail.com>
2018-10-11 11:25:33 +0300

    revert master to original state

--- renderers/adminRenderer.js
+++
@@ -1,7 +0,0 @@
-const { ipcRenderer } = require('electron');
-const ipc = ipcRenderer;
-
-const createAccount = document.getElementById('createAccountBtn');
-createAccount.addEventListener('click', ()=>{
-    ipc.send('open-create-account-window');
-});
